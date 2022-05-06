157e2e8085653eac2a06980a8f74220edde119b7
glacier1213 <78236306+glacier1213@users.noreply.github.com>
2022-05-06 15:31:37 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
-/** @type {import('next').NextConfig} */
+/** 
+  @type {import('next').NextConfig}
+*/
 const withTM = require("next-transpile-modules")(["@arcnovus/wet-boew-react"]); // pass the modules you would like to see transpiled
 const { i18n } = require("./next-i18next.config");
 const nextConfig = {
